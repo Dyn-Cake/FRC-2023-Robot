374aed4ec8c2aaf09d2199891d5d32923856c6ab
Cake <96643025+Full-Life-Alyx@users.noreply.github.com>
2023-02-07 15:33:05 -1000

    Added some tests

--- src/main/java/frc/robot/SmartDashboardUpdater.java
+++ src/main/java/frc/robot/SmartDashboardUpdater.java
@@ -1,5 +1,8 @@
 package frc.robot;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
@@ -16,6 +19,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.kauailabs.navx.frc.AHRS;
+import lombok.val;
 
 public class SmartDashboardUpdater {
     private long lastTriggered;
@@ -73,6 +77,15 @@ public class SmartDashboardUpdater {
             }
             chooser.addOption(type.name(), type);
         }
+
+        NetworkTableInstance instance = NetworkTableInstance.getDefault();
+        NetworkTable table = instance.getTable("Shuffleboard").getSubTable("MecanumDrive");
+// set the values for the Mecanum Drive widget
+        //Chat GPT test
+        table.getEntry("frontLeftSpeed").setDouble(0.1);
+        table.getEntry("frontRightSpeed").setDouble(0.2);
+        table.getEntry("rearLeftSpeed").setDouble(0.3);
+        table.getEntry("rearRightSpeed").setDouble(0.4);
     }
 
     public void update() {
@@ -93,17 +106,19 @@ public class SmartDashboardUpdater {
             );*/
         }
         
+
+        /*
         for (Spark motor : driveMotors.keySet()) {
             Shuffleboard.getTab("SmartDashboard")
             .add(motors.get(motor) + " voltage", motor.get())
             .withWidget(BuiltInWidgets.kMecanumDrive)
-            .withProperties(Map.of("min", -1, "max", 1))
+            //.withProperties(Map.of("min", -1, "max", 1))
             .getEntry();
             /*SmartDashboard.putNumber(
                     motors.get(motor) + " voltage",
                     motor.get()
-            );*/
-        }
+            );
+         }*/
     }
 
     private void updateGyro(){
