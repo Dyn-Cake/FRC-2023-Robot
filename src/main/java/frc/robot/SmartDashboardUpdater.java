199169d61992f4ca6acc5b82f3996aa42eb55257
Alyx <96643025+Full-Life-Alyx@users.noreply.github.com>
2023-02-10 15:24:45 -1000

    Revert "Add ObLog"

--- src/main/java/frc/robot/SmartDashboardUpdater.java
+++ src/main/java/frc/robot/SmartDashboardUpdater.java
@@ -1,9 +1,5 @@
 package frc.robot;
 
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.BuiltInAccelerometer;
-import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
@@ -22,23 +18,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.kauailabs.navx.frc.AHRS;
-import io.github.oblarg.oblog.Logger;
-import io.github.oblarg.oblog.annotations.Log;
-import lombok.val;
 
 public class SmartDashboardUpdater {
     private long lastTriggered;
-    private HashMap<Spark, String> motors;
+    private HashMap<Spark, Pair<GenericEntry, String>> motors;
+    private HashMap<Spark, String> driveMotors;
     private final SendableChooser<AutonomousPhaseType> chooser = new SendableChooser<>();
-    private final AHRS gyro;
-    private ShuffleboardTab tab = Shuffleboard.getTab("Shuffleboard");
-    private final GenericEntry gyroPitch;
-    //private final MecanumDrive mecanumDrive;
+    AHRS gyro;
+    private ShuffleboardTab tab = Shuffleboard.getTab("SmartDashboard");
+    private GenericEntry gyroPitch;
+    /*private NetworkTable limelight;
+    private NetworkTableEntry tx;
+    private NetworkTableEntry ty;
+    private NetworkTableEntry ta;*/
 
-
-    public SmartDashboardUpdater(HashMap<Integer, String> motors) {
+    public SmartDashboardUpdater(HashMap<Integer, String> motors, HashMap<Integer, String> driveMotors) {
         //Variables init only
-        //this.mecanumDrive = mecanumDrive;
         lastTriggered = System.currentTimeMillis();
 
         SparkMotorManager sparkMotorManager = SparkMotorManager.getInstance();
@@ -50,7 +45,14 @@ public class SmartDashboardUpdater {
         }
         this.motors = sparkMotors;
 
+        HashMap<Spark, String> driveSparkMotors = new HashMap<>();
+        for (Integer port : driveMotors.keySet()) {
+            driveSparkMotors.put(driveSparkMotorManager.getMotor(port), motors.get(port));
+        }
+        this.driveMotors = driveSparkMotors;
+
         gyro = new AHRS(SPI.Port.kMXP); /* Alternatives:  SPI.Port.kMXP, I2C.Port.kMXP or SerialPort.Port.kUSB */
+
         //limelight = NetworkTableInstance.getDefault().getTable("limelight");
 
         //logic
@@ -78,10 +80,10 @@ public class SmartDashboardUpdater {
         //ledMode = limelight.setEntry(2);
 
 
-        /*Shuffleboard.getTab(Constants.shuffleboardTabName)
+        /*Shuffleboard.getTab("SmartDashboard")
         .add("front left voltage", 1)
         .withWidget(BuiltInWidgets.kMecanumDrive)
-        .getEntry();
+        .getEntry();*/
         
         for (AutonomousPhaseType type : AutonomousPhaseType.values()) {
             //TODO un-hardcode this
@@ -93,16 +95,17 @@ public class SmartDashboardUpdater {
         }
         NetworkTableInstance instance = NetworkTableInstance.getDefault();
         NetworkTable table = instance.getTable("Shuffleboard").getSubTable("MecanumDrive");
-        // set the values for the Mecanum Drive widget
+// set the values for the Mecanum Drive widget
         //Chat GPT test
         table.getEntry("frontLeftSpeed").setDouble(0.1);
         table.getEntry("frontRightSpeed").setDouble(0.2);
         table.getEntry("rearLeftSpeed").setDouble(0.3);
         table.getEntry("rearRightSpeed").setDouble(0.4);
+        
     }
 
     public void update() {
-        //updateMotors();
+        updateMotors();
         updateDebug();
         updateGyro();
         //updateLimelight();
